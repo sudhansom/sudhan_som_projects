e7e17ccf27312c4e35314b5b65be05c28e08cb99
Sudhan <bkspoudel@gmail.com>
2019-10-26 10:45:46 +0200

    adding abstractExample to github

---
+++ AbstractExample.java
@@ -0,0 +1,35 @@
+package com.codewithme;
+
+abstract class Writer{
+    public abstract void write();
+}
+
+class Pen extends Writer{
+    public void write(){
+        System.out.println("i am a pen");
+    }
+}
+class Pencil extends Writer{
+    public void write(){
+        System.out.println("i am a pencil");
+    }
+}
+class Dotpen extends Writer{
+    public void write(){
+        System.out.println("i am a Dotpen");
+    }
+}
+class Kit{
+    public void display(Writer p){
+        p.write();
+    }
+}
+public class AbstractExample{
+    public static void main(String args[]){
+        Kit k = new Kit();
+        Writer p = new Pen();
+        Writer pc = new Pencil();
+        Writer dp = new Dotpen();
+        k.display(dp);
+    }
+}
