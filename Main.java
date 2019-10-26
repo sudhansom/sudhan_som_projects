24f8d8fd24ef347e6a0345901e64bd54f9dc47d8
Sudhan <bkspoudel@gmail.com>
2019-10-26 10:55:22 +0200

    added main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+package com.codewithme;
+
+abstract class LivingThings{
+    public abstract void walk();
+}
+class Buffallo extends LivingThings{
+    public void walk(){
+        System.out.println("Buffallo walks by 4 legs.");
+    }
+}
+class Penguin extends LivingThings{
+
+    public void walk() {
+        System.out.println("Penguin walks by two legs.");
+    }
+
+}
+ class Animal{
+    public void display(LivingThings living){
+        living.walk();
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("all about animals...");
+        Animal an = new Animal();
+	    LivingThings penguin = new Penguin();
+	    LivingThings buffallo = new Buffallo();
+	    penguin.walk();
+	    buffallo.walk();
+	    an.display(buffallo);
+    }
+}
